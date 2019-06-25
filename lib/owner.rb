0aa1acc317bee56bd99747416ef231e94e2763ac
Lukie Kang <lukiek+f@gmail.com>
2019-06-25 17:42:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -65,10 +65,15 @@ class Owner
   def sell_pets
     @cats.each do |cat|
       cat.mood = "nervous"
+      cat.owner = nil
     end
     @dogs.each do |dog|
       dog.mood = "nervous"
+      dog.owner = nil
     end
+    @cats = []
+    @dogs = []
+    
   end
 
 end # of owner
