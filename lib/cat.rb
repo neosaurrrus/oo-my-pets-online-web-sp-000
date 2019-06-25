8a94722dd621ee7cc12d928bf1da0fa81c2bf827
Lukie Kang <lukiek+f@gmail.com>
2019-06-25 17:38:33 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -7,6 +7,7 @@ attr_accessor :mood
   def initialize(name, owner, mood = "nervous")
     @name = name
     @mood = mood
+    @owner = owner
     @owner.cats << self
     # if owner
     # @owner = owner
