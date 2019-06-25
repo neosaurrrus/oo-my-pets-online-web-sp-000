017db907aea165807629db265fa27c2864acbbf3
Lukie Kang <lukiek+f@gmail.com>
2019-06-25 17:30:17 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,31 @@
 class Cat
-  # code goes here
+
+attr_reader :name
+attr_accessor :mood
+
+
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @mood = mood
+    @owner.cats << self
+  end
+
+ @@all = []
+
+ def self.all
+   @@all
+ end
+
+
+
+ def mood
+   @mood
+ end
+
+
+
+
+
+
+
 end
